2df8283fd821c0e7d9367622d9c1856af679dc05
Md. Shakhawat Hossen <shakhawat09@students.diu.ac>
2024-08-14 16:29:41 +0600

    change url local to live link

--- src/API/config.js
+++ src/API/config.js
@@ -1,10 +1,10 @@
 import { getToken } from "../helper/SessionHelper";
 
-const baseURL = "http://localhost:8000/api/v1";
-const imageBaseURL = "http://localhost:8000";
+// const baseURL = "http://localhost:8000/api/v1";
+// const imageBaseURL = "http://localhost:8000";
 
-// const baseURL = "https://glowify-backend.vercel.app/api/v1";
-// const imageBaseURL = "https://glowify-backend.vercel.app";
+const baseURL = "https://glowify-backend.vercel.app/api/v1";
+const imageBaseURL = "https://glowify-backend.vercel.app";
 
 const AxiosHeader = { headers: { Authorization: getToken() } };
 
